e5518508bfacf6ad2dae43d385b1ef58ee75f89f
abumaster <1902819397@qq.com>
2021-08-22 16:39:44 +0800

    :fire: 删除文件
    清理文件

--- spring-mq-demo/src/main/java/com/abumaster/springmqdemo/service/DemoService.java
+++
@@ -1,9 +0,0 @@
-package com.abumaster.springmqdemo.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class DemoService {
-
-
-}
--- ssm-demo/src/main/java/com/abumaster/ssmdemo/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.abumaster.ssmdemo.service;
-
-import com.abumaster.ssmdemo.entity.UserInfo;
-
-public interface UserService {
-    UserInfo getOne(String uid);
-}
